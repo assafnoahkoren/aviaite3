1b5cb3e0b617c342dc1101368f71dfd4052e367c
Assaf Koren <assafnoahkoren@gmail.com>
2025-06-09 11:02:02 +0300

    .

--- webapp/src/features/home-v2/useFirstTimeExperience.ts
+++ webapp/src/features/home-v2/useFirstTimeExperience.ts
@@ -5,27 +5,27 @@ import { useStore_Chat } from '../chat/chat-store';
 import type { Thread } from '../../api/chat-api';
 
 export function useFirstTimeExperience() {
-  const chatHistoryStore = useStore_ChatHistory();
-  const settingsStore = useStore_Settings();
-  const chatStore = useStore_Chat();
+	const chatHistoryStore = useStore_ChatHistory();
+	const settingsStore = useStore_Settings();
+	const chatStore = useStore_Chat();
 
-  useEffect(() => {
-    const hasChats = chatHistoryStore.chatsQuery.data && chatHistoryStore.chatsQuery.data.length > 0;
-    const isChatsLoading = chatHistoryStore.chatsQuery.isLoading;
-	const currentAssistant = settingsStore.assistants.find(a => a.id === settingsStore.assistants[0].id);
-	settingsStore.setCurrentAssistantId(currentAssistant?.id);
+	useEffect(() => {
+		const hasChats = chatHistoryStore.chatsQuery.data && chatHistoryStore.chatsQuery.data.length > 0;
+		const isChatsLoading = chatHistoryStore.chatsQuery.isLoading;
+		const currentAssistant = settingsStore.assistants.find(a => a.id === settingsStore.assistants[0].id);
 
-    if (!isChatsLoading && !hasChats && currentAssistant) {
-      chatHistoryStore.createChatMutation.mutateAsync({ assistantId: currentAssistant.id, profileId: currentAssistant.id })
-        .then((newChat: Thread) => {
-          chatStore.setCurrentChat(newChat);
-        });
-    }
-  }, [
-    chatHistoryStore.chatsQuery.data,
-    chatHistoryStore.chatsQuery.isLoading,
-    settingsStore.currentAssistant,
-    chatHistoryStore.createChatMutation,
-    chatStore,
-  ]);
+		if (!isChatsLoading && !hasChats && currentAssistant) {
+			settingsStore.setCurrentAssistantId(currentAssistant?.id);
+			chatHistoryStore.createChatMutation.mutateAsync({ assistantId: currentAssistant.id, profileId: currentAssistant.id })
+				.then((newChat: Thread) => {
+					chatStore.setCurrentChat(newChat);
+				});
+		}
+	}, [
+		chatHistoryStore.chatsQuery.data,
+		chatHistoryStore.chatsQuery.isLoading,
+		settingsStore.currentAssistant,
+		chatHistoryStore.createChatMutation,
+		chatStore,
+	]);
 } 
